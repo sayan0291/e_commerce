84003d806d22ce09eef55bb2f877dffcde64bc38
sayan-hue <sayanghanta57@gmail.com>
2026-02-18 11:45:47 +0530

    change made

---
+++ src/components/Home/Today/Today.jsx
@@ -0,0 +1,16 @@
+export default function Today(){
+
+    return(
+        <div>
+            <div>
+                //header
+                <div>
+                    
+                </div>
+            </div>
+            <div>
+                //items section
+            </div>
+        </div>
+    )
+}
